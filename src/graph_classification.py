f9ab916cf20ee47fd7fa2945b4d2adcafd79c647
CheshireCat12 <anthony.gillioz@outlook.com>
2022-09-28 09:41:01 +0200

    add new files in commit

---
+++ src/graph_classification.py
@@ -0,0 +1,85 @@
+import os
+import pathlib
+from typing import List, Tuple
+
+from graph_pkg_core.coordinator.coordinator import Coordinator
+
+from src.utils import set_global_verbose, Logger
+from src.utils import train_val_test_split
+from src.train_eval import train, evaluate
+
+def graph_classifier(root_dataset: str,
+                     parameters_edit_cost: Tuple,
+                     size_splits: List[float],
+                     alphas: List[float],
+                     ks: List[int],
+                     seed: int,
+                     n_cores: int,
+                     folder_results: str,
+                     save_predictions: bool,
+                     save_distances: bool,
+                     verbose: bool,
+                     args):
+    """
+
+    Args:
+        root_dataset:
+        parameters_edit_cost:
+        size_splits:
+        alphas:
+        ks:
+        seed:
+        n_cores:
+        folder_results:
+        save_predictions:
+        save_distances:
+        verbose:
+        args:
+
+    Returns:
+
+    """
+    set_global_verbose(verbose)
+
+    pathlib.Path(folder_results).mkdir(parents=True, exist_ok=True)
+
+    # Init logger
+    logger_filename = os.path.join(folder_results,
+                                   'results_general.json')
+    logger = Logger(logger_filename)
+
+    # Save all the input parameters
+    logger.data['parameters'] = vars(args)
+    logger.save_data()
+
+    coordinator = Coordinator(parameters_edit_cost,
+                              root_dataset)
+
+    # Split the dataset into train, val and test sets
+    size_train, size_val, size_test = size_splits
+    X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(coordinator.graphs,
+                                                                          coordinator.classes,
+                                                                          val_size=size_val,
+                                                                          test_size=size_test,
+                                                                          random_state=seed)
+
+    # Optimize the hyperparameters
+    acc_tracker = train(coordinator,
+                        logger,
+                        alphas, ks,
+                        X_train, X_val,
+                        list(y_train), list(y_val),
+                        n_cores)
+
+    # Merge train and validation set for the final evaluation
+    X_train = X_train + X_val
+    y_train = list(y_train) + list(y_val)
+
+    evaluate(coordinator,
+             logger,
+             acc_tracker,
+             X_train, X_test,
+             list(y_train), list(y_test),
+             n_cores,
+             folder_results,
+             save_predictions, save_distances)
---
+++ src/utils.py
@@ -0,0 +1,146 @@
+import csv
+import json
+from typing import Iterable, List
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def set_global_verbose(verbose: bool = False) -> None:
+    """
+    Set the global verbose.
+    Activate the logging module (use `logging.info('Hello world!')`)
+    Activate the tqdm loading bar.
+
+    Args:
+        verbose: If `True` activate the global verbose
+
+    Returns:
+
+    """
+    import logging
+    from functools import partialmethod
+    from tqdm import tqdm
+
+    if verbose:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        tqdm.__init__ = partialmethod(tqdm.__init__, disable=not verbose)
+
+
+def train_val_test_split(X: List,
+                         y: Iterable,
+                         val_size: float = 0.2,
+                         test_size: float = 0.2,
+                         random_state=1):
+    """
+
+    Args:
+        X:
+        y:
+        val_size:
+        test_size:
+        random_state:
+
+    Returns:
+
+    """
+    # First get the val split
+    X_train, X_val, y_train, y_val = train_test_split(X,
+                                                      y,
+                                                      test_size=val_size,
+                                                      random_state=random_state)
+
+    test_size = test_size / (1 - val_size)
+    X_train, X_test, y_train, y_test = train_test_split(X_train,
+                                                        y_train,
+                                                        test_size=test_size,
+                                                        random_state=random_state)
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+
+class Logger:
+
+    def __init__(self, filename: str):
+        """
+
+        Args:
+            filename:
+        """
+        self.filename = filename
+        self.__data = {}
+        self.lvl_name = None
+
+    @property
+    def data(self):
+        if self.lvl_name:
+            return self.__data[self.lvl_name]
+        else:
+            return self.__data
+
+    def set_lvl(self, lvl_name: str) -> None:
+        """
+        Change the level of the logger.
+        It is used to log experiment with multiple loops
+
+        Args:
+            lvl_name:
+
+        Returns:
+
+        """
+        self.lvl_name = lvl_name
+        self.__data[lvl_name] = {}
+
+    def save_data(self) -> None:
+        """
+        Save the current state of the data property.
+
+        Returns:
+
+        """
+        with open(self.filename, 'w') as file:
+            json.dump(self.__data, file, indent=4)
+
+
+def write_predictions(filename: str,
+                      predictions: List[int],
+                      GT_labels: List[int]) -> None:
+    """
+    Write the predictions and the corresponding GT labels in `filename`.
+
+    Args:
+        filename: File where to save the predictions.
+        predictions: Iterable of predictions
+        GT_labels: Iterable of the GT labels
+
+    Returns:
+
+    """
+
+    with open(filename, 'w') as csv_file:
+        fieldnames = ['predictions', 'GT_labels']
+
+        writer = csv.DictWriter(csv_file,
+                                fieldnames=fieldnames)
+
+        writer.writeheader()
+        for pred, GT_lbl in zip(predictions, GT_labels):
+            writer.writerow({'predictions': pred, 'GT_labels': GT_lbl})
+
+
+def write_distances(filename: str, distances: np.ndarray) -> None:
+    """
+    Save the GEDs in `.npy` file
+
+    Args:
+        filename: File where to save the GEDs.
+        distances: `np.array` containing the GEDs
+
+    Returns:
+
+    """
+    with open(filename, 'wb') as file:
+        np.save(file, distances)
+
